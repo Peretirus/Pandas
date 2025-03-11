9b9f26da18800f8cd5dcfad6deeb3f10ec10d57c
Peretirus <158774367+Peretirus@users.noreply.github.com>
2025-03-11 11:00:43 -0300

    Create 7-IFs_ELSEs.py

---
+++ 7-IFs_ELSEs.py
@@ -0,0 +1,15 @@
+# Defina variáveis
+room = "kit"
+area = 14.0
+
+# construção if-else para room
+if room == "kit" :
+print("olhando ao redor na cozinha.")
+else :
+print("olhando ao redor em outro lugar.")
+
+# construção if-else para area
+if area > 15 :
+print("lugar grande!")
+else:
+print("bem pequeno.")
