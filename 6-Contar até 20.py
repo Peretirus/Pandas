c695116b13748445f80dded416625196930b8881
Peretirus <158774367+Peretirus@users.noreply.github.com>
2025-03-11 14:28:14 -0300

    Update and rename 6-Enquanto(While).py to 6-Contar até 20.py

---
+++ 6-Contar até 20.py
@@ -0,0 +1,7 @@
+# Usuário define o valor inicial
+valor = int(input("Digite um número inicial: "))
+
+# Código while loop até 20
+while valor <= 20:
+    print(valor)  # Exibe os números na mesma linha
+    valor += 1
