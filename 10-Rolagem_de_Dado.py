4fe056bcc42124c75658308ae6d3a95dc9422b62
Peretirus <158774367+Peretirus@users.noreply.github.com>
2025-03-13 22:24:10 -0300

    Create 10-Rolagem_de_Dado.py

---
+++ 10-Rolagem_de_Dado.py
@@ -0,0 +1,11 @@
+# Import numpy and set seed
+import numpy as np
+np.random.seed()
+
+# Use randint() to simulate a dice
+rolar_dado1 = np.random.randint(1, 7)  # Gera um número entre 1 e 6
+print(rolar_dado1)
+
+# Use randint() again
+rolar_dado2 = np.random.randint(1, 7) 
+print(rolar_dado2)
